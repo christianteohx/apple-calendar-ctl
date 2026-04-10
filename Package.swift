9be68f747ad839cd74f397dff4b5eff4bdcf976e
Christian Teo <christeo2001@hotmail.com>
2026-04-10 18:11:12 -0500

    chore: broaden macOS version requirement to v13+

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
   name: "calctl",
-  platforms: [.macOS(.v14)],
+  platforms: [.macOS(.v13)],
   products: [
     .library(name: "CalendarCore", targets: ["CalendarCore"]),
     .executable(name: "calctl", targets: ["calctl"]),
